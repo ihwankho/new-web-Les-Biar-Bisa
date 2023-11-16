624a492603df7fb6e3b32411a364aa63f2e2c837
mallexibra <mallexibra@gmail.com>
2023-11-16 16:09:30 +0700

    feat: add payment page

--- resources/views/admin/page/payment/index.blade.php
+++ resources/views/admin/page/payment/index.blade.php
@@ -3,5 +3,37 @@
 @section('title', 'Admin Payment')
 
 @section('content')
-    <h1>Hello Admin Payment</h1>
+    <h5 class="page-title">Payment</h5>
+
+    <div>
+        <a href="#"
+            class="py-1 px-2 inline-block text-xs rounded-full border border-primary text-primary font-semibold">All</a>
+        <a href="#"
+            class="py-1 px-2 inline-block text-xs rounded-full border border-primary text-white bg-primary font-semibold">Come
+            In</a>
+        <a href="#"
+            class="py-1 px-2 inline-block text-xs rounded-full border border-primary text-primary font-semibold">Approved</a>
+        <a href="#"
+            class="py-1 px-2 inline-block text-xs rounded-full border border-primary text-primary font-semibold">Unapproved</a>
+    </div>
+
+    <div class="mt-5">
+        <div class="card">
+            <img class="card-thumbnail" src="{{ asset('/assets/course/thumbnail1.jpg') }}" alt="thumbnail-course">
+            <div class="flex my-2 justify-between items-center">
+                <h3 class="font-bold">From: Filfia Antika A.</h3>
+                <span class="text-[10px] font-semibold text-white py-1 px-2 rounded-full bg-primary/50">14 November
+                    2023</span>
+            </div>
+            <p class="text-xs">Bukti pembayaran bulan 1 les private</p>
+            <div class="mt-3 w-full flex gap-2">
+                <a href="#"
+                    class="p-1 block w-full text-xs font-medium text-white text-center bg-green-500 rounded-md">Edit
+                    Course</a>
+                <a href="#"
+                    class="p-1 block w-full text-xs font-medium text-white text-center bg-rose-500 rounded-md">Delete
+                    Course</a>
+            </div>
+        </div>
+    </div>
 @endsection
