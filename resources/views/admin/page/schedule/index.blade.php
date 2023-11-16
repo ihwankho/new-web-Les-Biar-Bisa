e8df044aa7ff25a3218af0b06611a83479ca0c31
mallexibra <mallexibra@gmail.com>
2023-11-16 11:19:15 +0700

    feat: add page schedule

--- resources/views/admin/page/schedule/index.blade.php
+++ resources/views/admin/page/schedule/index.blade.php
@@ -3,5 +3,32 @@
 @section('title', 'Admin Schedule')
 
 @section('content')
-    <h1>Hello Admin Schedule</h1>
+    <p class="page-title">Schedule Sekolah Dasar</p>
+    <div class="flex gap-3 items-center">
+        <div class="card-modify w-max mb-3">
+            <img width="426" src="{{ asset('/assets/schedule/schedule1.jpeg') }}" alt="schedule-sd">
+        </div>
+        <div class="space-y-3">
+            <span class="p-2 text-xs font-semibold bg-orange-500 flex gap-2 w-max text-white rounded-md"><img
+                    src="{{ asset('/assets/icon/exchange.svg') }}" alt="exchange-icon">
+                Change</span>
+            <span class="p-2 text-xs font-semibold bg-rose-500 flex gap-2 w-max text-white rounded-md"><img
+                    src="{{ asset('/assets/icon/remove.svg') }}" alt="remove-icon"> Remove</span>
+        </div>
+    </div>
+    <p class="page-title">Schedule Sekolah Menengah Pertama</p>
+    <a href="#" class="btn mb-3 inline-block">+ Add Schedule Image</a>
+    <p class="page-title">Schedule Sekolah Menengah Atas</p>
+    <div class="flex gap-3 items-center">
+        <div class="card-modify w-max mb-3">
+            <img width="426" src="{{ asset('/assets/schedule/schedule1.jpeg') }}" alt="schedule-sd">
+        </div>
+        <div class="space-y-3">
+            <span class="p-2 text-xs font-semibold bg-orange-500 flex gap-2 w-max text-white rounded-md"><img
+                    src="{{ asset('/assets/icon/exchange.svg') }}" alt="exchange-icon">
+                Change</span>
+            <span class="p-2 text-xs font-semibold bg-rose-500 flex gap-2 w-max text-white rounded-md"><img
+                    src="{{ asset('/assets/icon/remove.svg') }}" alt="remove-icon"> Remove</span>
+        </div>
+    </div>
 @endsection
